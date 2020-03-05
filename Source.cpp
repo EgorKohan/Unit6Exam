3a0732681c45e3dc3ac139d1e3a725466110f045
Egor <zhora.kokhan56@gmail.com>
2020-03-05 12:59:10 +0300

    Added Shop

--- Source.cpp
+++ Source.cpp
@@ -2,20 +2,7 @@
 
 using namespace std;
 
-class Student {
 
-	string name;
-	string surname;
-
-public:
-
-	Student() {};
-
-	Student(string name, string surname) :name(name), surname(surname) {};
-
-};
 
 int main() {
-	cout << "Hello";
-	Student *stu = new Student("Egor", "Kohan");
 }
